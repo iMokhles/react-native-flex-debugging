5d52fda242f0e7a87fe86ef5dd82d04dbc824c65
iMokhles <mokhleshussien@aol.com>
2020-10-16 18:17:27 +0200

    run methods in main threads

--- ios/FlexDebugging.m
+++ ios/FlexDebugging.m
@@ -7,17 +7,23 @@ RCT_EXPORT_MODULE()
 
 RCT_EXPORT_METHOD(showExplorer)
 {
+dispatch_async(dispatch_get_main_queue(), ^{
     [[IMLEXManager sharedManager] showExplorer];
+});
 }
 
 RCT_EXPORT_METHOD(hideExplorer)
 {
+dispatch_async(dispatch_get_main_queue(), ^{
     [[IMLEXManager sharedManager] hideExplorer];
+});
 }
 
 RCT_EXPORT_METHOD(toggleExplorer)
 {
+dispatch_async(dispatch_get_main_queue(), ^{
     [[IMLEXManager sharedManager] toggleExplorer];
+});
 }
 
 @end
